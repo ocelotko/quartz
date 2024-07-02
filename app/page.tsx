faaea43911e0f7efbbc99d60d7d9cf4ac7d8ca24
Jakub "Ocelot" Hachlinec <mcRestynSK@gmail.com>
2024-07-02 21:34:26 +0200

    Frontend revert

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,21 @@
-<<<<<<< HEAD
 import React from 'react'
 import List from './components/list/list';
 import Chat from './components/chat/chat';
 import Detail from './components/detail/detail';
-=======
-"use client";
->>>>>>> 61cb2633cd83d0675ef1d54daa5ab9309bc08722
-
-import React from 'react';
 import MessageForm from './MessageForm';
 
 const Home: React.FC = () => {
   return (
-<<<<<<< HEAD
     <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
-    </div>
-  )
-}
-=======
-    <div className="container">
       <header>
         <h1>Matrix Client</h1>
       </header>
-      <main>
-        <MessageForm />
-      </main>
+      <List/>
+      <Chat/>
+      <Detail/>
+      <MessageForm/>
     </div>
-  );
-};
->>>>>>> 61cb2633cd83d0675ef1d54daa5ab9309bc08722
+  )
+}
 
 export default Home;
