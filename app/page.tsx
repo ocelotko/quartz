61cb2633cd83d0675ef1d54daa5ab9309bc08722
shitkid22 <kidshit22@gmail.com>
2024-07-02 19:15:56 +0200

    temp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
 
-const HomePage = () => {
+import React from 'react';
+import MessageForm from './MessageForm';
+
+const Home: React.FC = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="container">
+      <header>
+        <h1>Matrix Client</h1>
+      </header>
+      <main>
+        <MessageForm />
+      </main>
+    </div>
+  );
+};
 
-export default HomePage
+export default Home;
